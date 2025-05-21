e8eb59fc8eaa323e9335c8a4cf5da75cb6db1227
eliamz1378 <elimz1378@gmail.com>
2025-05-21 11:52:59 +0330

    Added new JS and jQuery projects

---
+++ programiz/main.js
@@ -0,0 +1,8 @@
+import { message, number, multiplyNumbers } from './module.js';
+
+console.log(message); 
+
+console.log(number);
+
+console.log(multiplyNumbers(3, 4)); // 12
+console.log(multiplyNumbers(5, 8)); // 40
---
+++ programiz/module.js
@@ -0,0 +1,9 @@
+const message = 'hello world';
+
+const number = 10;
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+export { message, number, multiplyNumbers };
